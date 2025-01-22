522b5f6a140ca541bbde9dfdaf4b78cd08fea29b
Johannes Düsing <j.duesing13@googlemail.com>
2025-01-22 14:00:15 +0100

    Add scripts to run modular and WPA taint analyses for evaluation

---
+++ run_modular_taint_analysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if an argument is supplied
+if [ -z "$1" ]; then
+  echo "Usage: $0 <path_to_maven_project>"
+  exit 1
+fi
+
+PROJECT_PATH="$1"
+
+# Execute the docker command with the supplied path
+docker run -v "${PROJECT_PATH}:/app/input/" -v "${HOME}/.m2/:/root/.m2" --name sparri-modular-taint spar-analyses /app/input
